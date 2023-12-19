a99adb6334e070d58192323e172cd4284d19931e
alx_27 <didieralejandromejia83@gmail.com>
2023-12-19 14:24:32 -0500

    Logic for the creation of the cards

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-async function loaded(){
-    await fetch("https://rickandmortyapi.com/api/character?page=1")
+async function loaded(id = 1){
+    await fetch("https://rickandmortyapi.com/api/character?page=" + id)
     .then(res => res.json())
     .then(data => data.results)
     .then(data_f => localStorage.setItem("Characters", JSON.stringify(data_f)))
     .catch(() => false)
+
+    showCharacters()
 };
+
+async function showCharacters(){
+    if (localStorage.getItem("Characters")){
+        const characters = JSON.parse(localStorage.getItem("Characters"))
+        const dinamic_cards = document.getElementById("dinamic-cards");
+
+        const fragment = document.createDocumentFragment()
+
+        for (let character of characters){
+            const copy = document.getElementById("card-template").content.firstElementChild.cloneNode(true);
+
+            const img = copy.getElementsByTagName("img")[0];
+            img.setAttribute("src", character.image);
+
+            const name = copy.getElementsByTagName("h5")[0];
+            name.textContent = character.name;
+
+            const specie = copy.getElementsByTagName("p")[0];
+            specie.textContent = character.species;
+
+            fragment.appendChild(copy)
+        }
+
+        dinamic_cards.appendChild(fragment);
+    }
+}
+
 
 window.addEventListener("load", loaded, false);
